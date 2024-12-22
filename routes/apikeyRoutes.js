0a8b15963310bd08d221ca1611ddf5d93d15e375
finelabs-maker <vishaljuneja1001@gmail.com>
2024-12-22 21:46:28 +0530

    Added Razorpay payment

--- routes/apikeyRoutes.js
+++ routes/apikeyRoutes.js
@@ -1,15 +1,42 @@
 const express = require("express");
 const router = express.Router();
+const Razorpay = require("razorpay");
 const sgMail = require("@sendgrid/mail");
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-// Define the /apikey route
-router.get("/apikey/razorpay", (req, res) => {
-  res.send({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET,
-  });
+// Define the /payment endpoint
+router.post("/payment", async (req, res) => {
+  const { amount, currency, receipt } = req.body;
+
+  try {
+    // Create an order with Razorpay
+    const options = {
+      amount: amount,
+      currency: currency || "INR",
+      receipt: receipt || "receipt#1",
+      payment_capture: 1,
+    };
+
+    const order = await razorpay.orders.create(options);
+    res.status(200).json({
+      success: true,
+      message: "Order created successfully",
+      order,
+    });
+  } catch (error) {
+    console.error("Error creating payment:", error);
+    res.status(500).json({
+      success: false,
+      message: "Error creating payment",
+      error,
+    });
+  }
 });
 
 // Email Sending Endpoint
